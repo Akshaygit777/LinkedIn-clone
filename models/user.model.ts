bd362bbb1e0ff871aaac237520c6526bb450f2e8
Akshaygit777 <cr380019@gmail.com>
2025-09-18 16:19:41 +0530

    connected mongodb and created user model

---
+++ models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Model } from "mongoose";
+export interface IUser{
+    firstName:string,
+    lastName:string
+    userId:string,
+    profilePhoto?:string,
+    bio?:string
+}
+export interface IUserDocument extends IUser , Document{
+    createdAt : Date,
+    updatedAt : Date
+} 
+const userSchema = new mongoose.Schema<IUserDocument>({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:String,
+        required:true
+    },
+    profilePhoto:{
+        type:String,
+        Default:""
+
+    },
+    bio:{
+        type:String,
+        required:""
+    }
+},{timestamps:true});
+export const User: Model<IUserDocument> = mongoose.models?.User || mongoose.model<IUserDocument>("User",userSchema);
